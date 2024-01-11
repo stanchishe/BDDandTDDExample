ee4b0e881b859ccd8bfa830cd0af896a9f0782c9
stanc <stanko_uzunov@yahoo.com>
2024-01-11 21:38:23 +0100

    Add tests for the new calculator scenario

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import gherkin.lexer.Ca;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.junit.jupiter.api.Assertions;
@@ -8,37 +7,26 @@ import org.junit.jupiter.api.Assertions;
 public class CalculatorTest {
     private static Calculator calculator;
     private int result;
-    private int first, second;
     @Before
     public static void init() {
         calculator = new Calculator();
     }
 
-    @Given("^the numbers (\\d+) and (\\d+)$")
-    public void theAdditionIntegersAre(Integer firstInteger, Integer secondInteger) {
-        first = firstInteger;
-        second = secondInteger;
-    }
     @When("I add the two integers")
     public void iAddTheIntegers() {
-        result = calculator.add(first, second);
+        result = calculator.add(InitialSetup.first, InitialSetup.second);
     }
-    @Then("the result should be {int}")
+    @Then("the sum result should be {int}")
     public void theAdditionResultIs(Integer expectedResult) {
         Assertions.assertEquals(expectedResult, result, "The addition was wrong!!!");
     }
 
-//    @Given("^the numbers (\\d+) and (\\d+)$")
-//    public void theSubtractionIntegersAre(Integer firstInteger, Integer secondInteger) {
-//        first = firstInteger;
-//        second = secondInteger;
-//    }
-//    @When("I subtract the two integers")
-//    public void iSubtractTheIntegers() {
-//        result = calculator.subtract(first, second);
-//    }
-//    @Then("the result should be {int}")
-//    public void theSubtractionResultIs(Integer expectedResult) {
-//        Assertions.assertEquals(expectedResult, result, "The subtraction was wrong!!!");
-//    }
+    @When("I subtract the two integers")
+    public void iSubtractTheIntegers() {
+        result = calculator.subtract(InitialSetup.first, InitialSetup.second);
+    }
+    @Then("the subtract result should be {int}")
+    public void theSubtractionResultIs(Integer expectedResult) {
+        Assertions.assertEquals(expectedResult, result, "The subtraction was wrong!!!");
+    }
 }
